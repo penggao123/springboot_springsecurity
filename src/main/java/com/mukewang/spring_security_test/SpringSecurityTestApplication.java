872e513aead0f04c464f0dcefeb7881ce8eaf93a
langzi <1340852254@qq.com>
2020-11-09 20:18:16 +0800

    添加测试方法测试项目是否启动正常

--- src/main/java/com/mukewang/spring_security_test/SpringSecurityTestApplication.java
+++ src/main/java/com/mukewang/spring_security_test/SpringSecurityTestApplication.java
@@ -2,12 +2,33 @@ package com.mukewang.spring_security_test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringSecurityTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityTestApplication.class, args);
 	}
+
+
+	@GetMapping("/")
+	public String hello(){
+
+		return "helle";
+
+	}
+
+
+	@GetMapping("/login")
+	public String home(){
+
+		return "success  login";
+
+	}
+
+
 
 }
